4ec90cd8b4e42af819e8a6ff500d189570389a47
GadhiyaVatsal <gadhiya.vatsal007@gmail.com>
2023-11-12 12:36:07 -0500

    iOS translation history

---
+++ iosApp/iosApp/translate/presentation/components/TranslateHistoryItem.swift
@@ -0,0 +1,19 @@
+//
+//  TranslateHistory.swift
+//  iosApp
+//
+//  Created by Vatsal Gadhiya on 2023-11-12.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TranslateHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslateHistory()
+}
